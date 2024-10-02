5783e4b8559098ce5b7eb83f27bf48b233e74c6e
Nurul Amin <namin2750@gmail.com>
2024-10-02 19:06:24 +0600

    dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -1,9 +1,18 @@
-
-
-const layout = () => {
-  return (
-    <div>layout</div>
-  )
+export default function DashboardLayout({
+    children,
+  }: Readonly<{
+    children: React.ReactNode;
+  }>) {
+    return (
+      <div className="h-screen flex">
+      {/* LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] p-4">
+       left
+      </div>
+      {/* RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll flex flex-col">
+       right
+      </div>
+    </div>
+  );
 }
-
-export default layout
