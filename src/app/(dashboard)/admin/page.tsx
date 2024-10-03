5da48d9d5f2d1e6e253b4735e90b3cb6d73a02a5
Nurul Amin <namin2750@gmail.com>
2024-10-03 21:35:52 +0600

    count charts design

--- src/app/(dashboard)/admin/page.tsx
+++ src/app/(dashboard)/admin/page.tsx
@@ -1,18 +1,30 @@
+import CountChart from "@/components/CountChart";
 import UserCard from "@/components/UserCard";
 
 const AdminPage = () => {
   return (
     <div className="p-4 flex gap-4 flex-col md:flex-row">
-      {/* left */}
-      <div className="w-full lg:w-2/3">
+      {/* Left */}
+      <div className="w-full lg:w-2/3 flex flex-col gap-8">
         <div className="flex gap-4 justify-between flex-wrap">
           <UserCard type="Stdents" />
           <UserCard type="Teachers" />
           <UserCard type="Parents" />
           <UserCard type="staffs" />
         </div>
+        {/* Middle Chart */}
+        <div className="flex gap-4 flex-col lg:flex-row">
+          {/* Count harts */}
+          <div className="w-full lg:w-1/3 h-[450px]">
+            <CountChart />
+          </div>
+          {/* Attendence Chart */}
+          <div className="w-full lg:w-2/3 h-[450px]"></div>
+        </div>
+        {/* Bottom Chart */}
+        <div className=""></div>
       </div>
-      {/* right */}
+      {/* Right */}
       <div className="w-full lg:w-1/3">RRRRR</div>
     </div>
   );
