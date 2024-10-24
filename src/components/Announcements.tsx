75a0b7790fb8c52e7ae5352708552e83d561ed49
Nurul Amin <namin2750@gmail.com>
2024-10-24 21:11:54 +0600

    Announce Comp

---
+++ src/components/Announcements.tsx
@@ -0,0 +1,12 @@
+const Announcements = () => {
+  return (
+    <div className="bg-white p-4 rounded-md">
+      <div className="flex items-center justify-between">
+        <h1>Announcements</h1>
+        <span className="text-xs text-gray-400 cursor-pointer">View All</span>
+      </div>
+    </div>
+  );
+};
+
+export default Announcements;
