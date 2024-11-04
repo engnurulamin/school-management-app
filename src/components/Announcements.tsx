2d24d421f9fa228f20775eb2156ae16238334948
Nurul Amin <namin2750@gmail.com>
2024-11-04 21:23:12 +0600

    announcement comp

--- src/components/Announcements.tsx
+++ src/components/Announcements.tsx
@@ -2,8 +2,46 @@ const Announcements = () => {
   return (
     <div className="bg-white p-4 rounded-md">
       <div className="flex items-center justify-between">
-        <h1>Announcements</h1>
+        <h1 className="text-xl font-semibold">Announcements</h1>
         <span className="text-xs text-gray-400 cursor-pointer">View All</span>
+      </div>
+      <div className="flex flex-col gap-4 mt-4">
+        <div className="bg-SkyLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem Ipsum dollar sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+              11-05-2024
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sunt
+            voluptate quia sapiente libero rem.
+          </p>
+        </div>
+        <div className="bg-PurpleLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem Ipsum dollar sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+              11-05-2024
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sunt
+            voluptate quia sapiente libero rem.
+          </p>
+        </div>
+        <div className="bg-YellowLight rounded-md p-4">
+          <div className="flex items-center justify-between">
+            <h2 className="font-medium">Lorem Ipsum dollar sit</h2>
+            <span className="text-xs text-gray-400 bg-white rounded-md p-1">
+              11-05-2024
+            </span>
+          </div>
+          <p className="text-sm text-gray-400 mt-2">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Dicta sunt
+            voluptate quia sapiente libero rem.
+          </p>
+        </div>
       </div>
     </div>
   );
