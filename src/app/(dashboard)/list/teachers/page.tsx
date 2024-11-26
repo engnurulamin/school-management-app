e691d7a453cdc7ed3b3084120843fd15d157809f
Nurul Amin <namin2750@gmail.com>
2024-11-26 20:10:38 +0600

    Pagination comp

--- src/app/(dashboard)/list/teachers/page.tsx
+++ src/app/(dashboard)/list/teachers/page.tsx
@@ -1,3 +1,4 @@
+import Pagination from "@/components/Pagination";
 import TableSearch from "@/components/TableSearch";
 import Image from "next/image";
 
@@ -8,15 +9,21 @@ const TeacherListPage = () => {
         <h1 className="hidden md:block text-lg font-semibold">All Teachers</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
-          <div className="">
-            <button>
+          <div className="flex items-center gap-4 self-end">
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-Yellow">
               <Image src="/filter.png" alt="Filter" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-Yellow">
+              <Image src="/sort.png" alt="Filter" width={14} height={14} />
+            </button>
+            <button className="w-8 h-8 flex items-center justify-center rounded-full bg-Yellow">
+              <Image src="/plus.png" alt="Filter" width={14} height={14} />
             </button>
           </div>
         </div>
       </div>
       <div className=""></div>
-      <div className=""></div>
+      <Pagination />
     </div>
   );
 };
---
+++ src/components/Pagination.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+const Pagination = () => {
+  return (
+    <div className="flex items-center justify-between p-4 text-gray-500">
+      <button
+        disabled
+        className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed"
+      >
+        Prev
+      </button>
+      <div className="flex items-center gap-2 text-sm">
+        <button className="py-2 px-4 rounded-md bg-Sky text-xs font-semibold">
+          1
+        </button>
+        <button className="py-2 px-4 rounded-md ">2</button>
+        <button className="py-2 px-4 rounded-md ">3</button>
+        <button className="py-2 px-4 rounded-md ">4</button>
+      </div>
+      <button className="py-2 px-4 rounded-md bg-slate-200 text-xs font-semibold disabled:opacity-50 disabled:cursor-not-allowed">
+        Next
+      </button>
+    </div>
+  );
+};
+
+export default Pagination;
