6452f7b44eefe46325ab95263cd6fe73cfe2470a
Nurul Amin <namin2750@gmail.com>
2024-11-28 00:17:32 +0600

    exam page UI

--- src/app/(dashboard)/list/exams/page.tsx
+++ src/app/(dashboard)/list/exams/page.tsx
@@ -1,36 +1,35 @@
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
-import { classesData, role } from "@/lib/data";
+import {  examsData, role } from "@/lib/data";
 import Image from "next/image";
 import Link from "next/link";
 
-type Class = {
+type Exam = {
   id: number;
-  name: string;
-  capacity: number;
-  grade: number;
-  supervisor: string;
+  subject: string;
+  class: string;
+  teacher: string;
+  date: string;
 };
 
 const columns = [
   {
-    header: "Class Name",
+    header: "Subject Name",
     accessor: "name",
   },
   {
-    header: "Capacity",
-    accessor: "capacity",
+    header: "Class",
+    accessor: "class",
+  },
+  {
+    header: "Teacher",
+    accessor: "teacher",
     className: "hidden md:table-cell",
   },
   {
-    header: "Grade",
-    accessor: "grade",
-    className: "hidden md:table-cell",
-  },
-  {
-    header: "Supervisor",
-    accessor: "supervisor",
+    header: "Date",
+    accessor: "date",
     className: "hidden md:table-cell",
   },
   {
@@ -39,26 +38,26 @@ const columns = [
   },
 ];
 
-const ClassListPage = () => {
-  const renderRow = (item: Class) => (
+const ExamsListPage = () => {
+  const renderRow = (item: Exam) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-PurpleLight"
     >
-      <td className="flex items-center gap-4 p-4">{item.name}</td>
-      <td className="hidden md:table-cell">{item.capacity}</td>
-      <td className="hidden md:table-cell">{item.grade}</td>
-      <td className="hidden md:table-cell">{item.supervisor}</td>
+      <td className="flex items-center gap-4 p-4">{item.subject}</td>
+      <td className="hidden md:table-cell">{item.class}</td>
+      <td className="hidden md:table-cell">{item.teacher}</td>
+      <td className="hidden md:table-cell">{item.date}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/classes/${item.id}`}>
+          <Link href={`/list/exams/${item.id}`}>
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-Sky">
-              <Image src="/view.png" alt="" width={16} height={16} />
+              <Image src="/view.png" alt="View" width={16} height={16} />
             </button>
           </Link>
           {role === "admin" && (
             <button className="w-7 h-7 flex items-center justify-center rounded-full bg-Purple">
-              <Image src="/delete.png" alt="" width={16} height={16} />
+              <Image src="/delete.png" alt="Delete" width={16} height={16} />
             </button>         
           )}
         </div>
@@ -70,30 +69,30 @@ const ClassListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Classes</h1>
+        <h1 className="hidden md:block text-lg font-semibold">All Exams</h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-Yellow">
-              <Image src="/filter.png" alt="" width={14} height={14} />
+              <Image src="/filter.png" alt="Filter" width={14} height={14} />
             </button>
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-Yellow">
-              <Image src="/sort.png" alt="" width={14} height={14} />
+              <Image src="/sort.png" alt="Sort" width={14} height={14} />
             </button>
             {role === "admin" && (
 							<button className="w-8 h-8 flex items-center justify-center rounded-full bg-Yellow">
-								<Image src="/plus.png" alt="" width={14} height={14} />
+								<Image src="/plus.png" alt="Add" width={14} height={14} />
             	</button> 
 						)}
           </div>
         </div>
       </div>
       {/* LIST */}
-      <Table columns={columns} renderRow={renderRow} data={classesData} />
+      <Table columns={columns} renderRow={renderRow} data={examsData} />
       {/* PAGINATION */}
       <Pagination />
     </div>
   );
 };
 
-export default ClassListPage;
+export default ExamsListPage;
