8f99d447badd6cf41c2ff9b8f806854dd35d13cd
Nurul Amin <namin2750@gmail.com>
2024-12-03 19:02:33 +0600

    fix

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -14,25 +14,23 @@ const SingleTeacherPage = () => {
             </div>
             <div className="w-2/3 flex flex-col justify-between gap-4">
               <h1 className="text-xl font-semibold">Nurul Amin</h1>
-              <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Obcaecati maiores quisquam accusamus corrupti enim.
-              </p>
+              <p className="text-sm text-gray-500">Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
               <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/blood.png" alt="Blood" width={14} height={14} />
-                  <span>A+</span>
+                  <span className="whitespace-nowrap">A+</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/date.png" alt="Blood" width={14} height={14} />
-                  <span>Jan 2025</span>
+                  <span className="whitespace-nowrap">12-05-2024</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/mail.png" alt="Mail" width={14} height={14} />
-                  <span>user@mail.com</span>
+                  <span className="whitespace-nowrap">user@mail.com</span>
                 </div>
                 <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center gap-2">
                   <Image src="/phone.png" alt="Phone" width={14} height={14} />
-                  <span>01712 568741</span>
+                  <span className="whitespace-nowrap">01712 568741</span>
                 </div>
               </div>
             </div>
