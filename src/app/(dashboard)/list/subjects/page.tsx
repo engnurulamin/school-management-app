70da495735702c56326514b12836eb33fafafa23
Nurul Amin <namin2750@gmail.com>
2024-12-05 18:52:42 +0600

    fix

--- src/app/(dashboard)/list/subjects/page.tsx
+++ src/app/(dashboard)/list/subjects/page.tsx
@@ -40,11 +40,6 @@ const SubjectsListPage = () => {
       <td className="hidden md:table-cell">{item.teachers.join(",")}</td>
       <td>
         <div className="flex items-center gap-2">
-          <Link href={`/list/subjects/${item.id}`}>
-            <button className="w-7 h-7 flex items-center justify-center rounded-full bg-Sky">
-              <Image src="/view.png" alt="" width={16} height={16} />
-            </button>
-          </Link>
           {role === "admin" && (
             <>
               <FormModal table="subject" type="update" data={item} />
