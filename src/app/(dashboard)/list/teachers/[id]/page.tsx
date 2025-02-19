712f70b3aafddd4ec761bf9c4d1c4813bcf66e5b
Nurul Amin <namin2750@gmail.com>
2025-02-19 21:25:33 +0600

    temp link

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -138,7 +138,10 @@ const SingleTeacherPage = () => {
             <Link className="p-3 rounded-md bg-Sky" href="/">
               Teacher&apos;s Classes
             </Link>
-            <Link className="p-3 rounded-md bg-PurpleLight" href="/">
+            <Link
+              className="p-3 rounded-md bg-PurpleLight"
+              href={`/list/students/?teacherId=${"teacher2"}`}
+            >
               Teacher&apos;s Students
             </Link>
             <Link className="p-3 rounded-md bg-YellowLight" href="/">
