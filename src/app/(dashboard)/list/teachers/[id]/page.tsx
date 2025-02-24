98196620ca623e5f9ab79ca5e4eb08bd342a760f
Nurul Amin <namin2750@gmail.com>
2025-02-24 22:00:24 +0600

    lesson page

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -147,7 +147,10 @@ const SingleTeacherPage = () => {
             >
               Teacher&apos;s Students
             </Link>
-            <Link className="p-3 rounded-md bg-YellowLight" href="/">
+            <Link
+              className="p-3 rounded-md bg-YellowLight"
+              href={`/list/lessons?teacherId=${"teacher2"}`}
+            >
               Teacher&apos;s Lessons
             </Link>
             <Link className="p-3 rounded-md bg-pink-50" href="/">
